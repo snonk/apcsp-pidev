4bd3d6d0c656d17e9cc61468a8338bc77c485ea5
Selena Qiao <selena.c.qiao@gmail.com>
2020-01-07 12:53:08 -0800

    Changed arrays.c

--- arrays.c
+++ arrays.c
@@ -3,7 +3,6 @@
 void arrayAdd(int arr[], int s, int n) {
 	for (int i = 0; i < s; i++) {
 		*(arr+i) = *(arr+i) + n;
-		printf("%d\t", *(arr+i));
 	}
 }
 
@@ -21,6 +20,11 @@ for (int i = 0; i < 100; i++) {
 //Runs arrayAdd function and prints.
 printf("\nNew Array:\n");
 arrayAdd(array, 100, 5);
+
+for (int i = 0; i < 100; i++) {
+	printf("%d\t", *(array+i));
+}
+
 printf("\n");
 
 }
