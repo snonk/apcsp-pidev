463a48996bd81c2a69adb210a3811794d736e256
snonk <58750937+snonk@users.noreply.github.com>
2020-01-10 22:57:03 -0800

    Added studentmain.c

---
+++ mfile/studentmain.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include "student.h"
+
+int main(){
+
+  int studentcounter = 0;
+  struct student studentarray[50]; 
+
+  while(1){
+    char charthing[50];
+
+    //Student Name
+
+    printf("Input student first name: ");
+    fgets(studentarray[studentcounter].firstname, 256, stdin);
+
+    printf("Input student last name: ");
+    fgets(studentarray[studentcounter].lastname, 256, stdin);
+
+    //Student age
+
+    printf("Input student age: ");
+    fgets(charthing, 256, stdin);
+    while (sscanf(charthing, "%ld", &studentarray[studentcounter].age) == 0) {
+        printf("Not a valid age - try again!\nInput student age: ");
+       fgets(charthing, 256, stdin); 
+    }
+    sscanf(charthing, "%ld", &studentarray[studentcounter].age);
+
+    //Student ID
+
+    printf("Input student ID: ");
+    fgets(charthing, 256, stdin);
+    while (sscanf(charthing, "%ld", &studentarray[studentcounter].studentid) == 0) {
+    
+        printf("Not a valid student ID - try again!\nInput student ID: ");
+        fgets(charthing, 256, stdin);
+    }
+    sscanf(charthing, "%ld", &studentarray[studentcounter].studentid);
+
+    printf("Would you like to input more students? (y/n)\n");
+    fgets(charthing, 256, stdin);
+
+    while ('y' != charthing[0] && charthing[0] !='n' && charthing[0] != 'Y' && charthing[0] != 'N') {
+        printf("Please answer y or n.\n");
+        fgets(charthing, 256, stdin);
+    }
+
+    if (charthing[0] == 'n' || charthing[0] == 'N') {
+      for (int i = 0; i <= studentcounter; i++) {
+        printf("\n--- Student %d ---\n", i + 1);
+        printStudent(&studentarray[i]);
+      }
+      break;
+    }
+
+    studentcounter++;
+
+    if (charthing[0] == 'y' ||charthing[0] == 'Y') {
+      struct student studentarray[studentcounter];
+      continue;
+    }
+    
+    printf("%s", studentarray[0].firstname);
+
+  }
+}
